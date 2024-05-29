7dd4f0a6c80409842be0ebcf71b383382de1346b
alexandresaints <alexandresaintss@gmail.com>
2024-05-29 03:00:51 -0300

    update

--- src/database/db.js
+++ src/database/db.js
@@ -7,5 +7,5 @@ const dbUser = process.env.DB_USER
 const dbPass = process.env.DB_PASS
 
 export const connectDatabase = async () => {
-    await mongoose.connect(`mongodb+srv://alekplaysholy:SVswufPC9Ruc2x10@holy.aylm8rn.mongodb.net/?retryWrites=true&w=majority&appName=Holy`)
+    await mongoose.connect(`mongodb+srv://alekplaysholy:SkFpl1yKVB0ltVfL@holy.aylm8rn.mongodb.net/?retryWrites=true&w=majority&appName=Holy`)
 }
